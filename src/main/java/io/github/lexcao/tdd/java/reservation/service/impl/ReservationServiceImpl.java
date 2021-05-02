52eb930704cfd2b47550e260221873bf8fb7b0af
Lex Cao <lexcao@foxmail.com>
2021-05-02 23:45:18 +0800

    finish java-red-02

--- src/main/java/io/github/lexcao/tdd/java/reservation/service/impl/ReservationServiceImpl.java
+++ src/main/java/io/github/lexcao/tdd/java/reservation/service/impl/ReservationServiceImpl.java
@@ -2,9 +2,15 @@ package io.github.lexcao.tdd.java.reservation.service.impl;
 
 
 import io.github.lexcao.tdd.java.reservation.entity.Reservation;
+import io.github.lexcao.tdd.java.reservation.repository.ReservationRepository;
 import io.github.lexcao.tdd.java.reservation.service.ReservationService;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class ReservationServiceImpl implements ReservationService {
+
+    private final ReservationRepository repository;
+
     @Override
     public Reservation makeReservation(Reservation reservation) {
         return reservation;
