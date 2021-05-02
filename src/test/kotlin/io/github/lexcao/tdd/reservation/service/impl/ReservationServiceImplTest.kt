b29c4f9616a0316ee05829d3399ecf8dde897655
Lex Cao <lexcao@foxmail.com>
2021-05-02 22:52:00 +0800

    finish kotlin-red-02

--- src/test/kotlin/io/github/lexcao/tdd/reservation/service/impl/ReservationServiceImplTest.kt
+++ src/test/kotlin/io/github/lexcao/tdd/reservation/service/impl/ReservationServiceImplTest.kt
@@ -1,8 +1,12 @@
 package io.github.lexcao.tdd.reservation.service.impl
 
 import io.github.lexcao.tdd.reservation.entity.Reservation
+import io.github.lexcao.tdd.reservation.repository.ReservationRepository
 import io.github.lexcao.tdd.reservation.service.ReservationService
 import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verifySequence
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -11,7 +15,8 @@ import java.time.LocalDateTime
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class ReservationServiceImplTest {
 
-    private val service: ReservationService = ReservationServiceImpl()
+    private val mockRepository: ReservationRepository = mockk()
+    private val service: ReservationService = ReservationServiceImpl(mockRepository)
 
     @Nested
     inner class MakeReservation {
@@ -22,9 +27,15 @@ internal class ReservationServiceImplTest {
         fun shouldSuccess() {
             // given
             val reservation = Reservation(name = "Tom", time = time)
+            every { mockRepository.save(any()) } returns reservation
 
             // actual
             val reserved: Reservation = service.makeReservation(reservation)
+
+            // verify
+            verifySequence {
+                mockRepository.save(reservation)
+            }
 
             // expect
             reserved shouldBe reservation
