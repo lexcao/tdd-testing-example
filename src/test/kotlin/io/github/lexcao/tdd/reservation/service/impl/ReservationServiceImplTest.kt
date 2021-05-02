ff543c7062ca481196cc07c42cb84ffd7655bcb0
Lex Cao <lexcao@foxmail.com>
2021-05-02 22:30:07 +0800

    finish kotlin-red-01

--- src/test/kotlin/io/github/lexcao/tdd/reservation/service/impl/ReservationServiceImplTest.kt
+++ src/test/kotlin/io/github/lexcao/tdd/reservation/service/impl/ReservationServiceImplTest.kt
@@ -1,20 +1,15 @@
 package io.github.lexcao.tdd.reservation.service.impl
 
 import io.github.lexcao.tdd.reservation.entity.Reservation
-import io.github.lexcao.tdd.reservation.repository.ReservationRepository
 import io.github.lexcao.tdd.reservation.service.ReservationService
 import io.kotest.matchers.shouldBe
-import io.mockk.mockk
-import io.mockk.verifySequence
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 
 internal class ReservationServiceImplTest {
 
-    private val mockRepo: ReservationRepository = mockk()
-
-    private val service: ReservationService = ReservationServiceImpl(mockRepo)
+    private val service: ReservationService = ReservationServiceImpl()
 
     @Nested
     inner class MakeReservation {
@@ -28,12 +23,6 @@ internal class ReservationServiceImplTest {
 
             // actual
             val reserved: Reservation = service.makeReservation(reservation)
-
-            // then verify
-            verifySequence {
-                mockRepo.findByTime(time)
-                mockRepo.save(reservation)
-            }
 
             // expect
             reserved shouldBe reservation
