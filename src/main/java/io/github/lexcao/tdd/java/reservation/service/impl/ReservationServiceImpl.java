1430f96af95745b4d067457dbfd28ff18f3f5fe3
Lex Cao <lexcao@foxmail.com>
2021-05-02 23:37:23 +0800

    finish java-green-01

--- src/main/java/io/github/lexcao/tdd/java/reservation/service/impl/ReservationServiceImpl.java
+++ src/main/java/io/github/lexcao/tdd/java/reservation/service/impl/ReservationServiceImpl.java
@@ -7,6 +7,6 @@ import io.github.lexcao.tdd.java.reservation.service.ReservationService;
 public class ReservationServiceImpl implements ReservationService {
     @Override
     public Reservation makeReservation(Reservation reservation) {
-        return null;
+        return reservation;
     }
 }
